6dc16f27f11e2029ac6aa4c9c30c6c3f748dba6e
DESKTOP-HHJ9SIF\user <shg99sora@yahoo.co.jp>
2019-01-03 18:50:57 +0900

    簡単なデータ取得

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('foo/foo1', 'FooController@foo1');
+Route::get('foo/foo2', 'FooController@foo2');
+Route::get('foo/foo3', 'FooController@foo3');
