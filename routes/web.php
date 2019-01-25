3045df5b8d91c42d35d3b6a1be8c7dacf3d4824d
DESKTOP-HHJ9SIF\user <shg99sora@yahoo.co.jp>
2019-01-26 01:49:38 +0900

    vueのはろわ。

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
+// APIのURL以外のリクエストに対してはindexテンプレートを返す
+// 画面遷移はフロントエンドのVueRouterが制御する
+Route::get('/{any?}', function () {
+    return view('index');
+    }
+)->where('any', '.+');
+
 Route::get('/feat', 'FeatController@index01');
 
 
