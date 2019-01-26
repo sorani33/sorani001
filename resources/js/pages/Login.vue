ee04449d717520e7da9bf0b71916c365f37439ba
DESKTOP-HHJ9SIF\user <shg99sora@yahoo.co.jp>
2019-01-26 16:47:06 +0900

    ログインフォームの実装。

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -5,8 +5,20 @@
       <li class="tab__item" :class="{'tab__item--active': tab === 2 }" @click="tab = 2"> Register </li>
     </ul>
     {{ tab }}
-    <div class="panel" v-show="tab === 1">Login Form</div>
-    <div class="panel" v-show="tab === 2">Register Form</div>
+    <div class="panel" v-show="tab === 1">
+        <form class="form" @submit.prevent="login">
+            <label for="login-email">Email</label>
+            <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
+            <label for="login-password">Password</label>
+            <input type="password" class="form__item" id="login-password" v-model="loginForm.password">
+            <div class="form__button">
+              <button type="submit" class="button button--inverse">login</button>
+            </div>
+        </form>
+    </div>
+    <div class="panel" v-show="tab === 2">
+
+    </div>
   </div>
 </template>
 
@@ -14,7 +26,17 @@
 export default {
   data () {
     return {
-      tab: 1
+      tab: 1,
+      loginForm: {
+        email: '',
+        password: ''
+      },
+    }
+  },
+
+  methods: {
+    login () {
+      console.log(this.loginForm)
     }
   }
 }
