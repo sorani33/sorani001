89cf51926ba8f740763e258438d7bde7cca454a8
DESKTOP-HHJ9SIF\user <shg99sora@yahoo.co.jp>
2019-01-26 13:11:53 +0900

    会員登録テスト

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -66,7 +66,8 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('api')
-             ->middleware('api')
+             // ->middleware('api')
+             ->middleware('web')  //SPAのため変更
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
     }
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
+// 会員登録
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
