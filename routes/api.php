e16e9ae989008370e4e64cb39f62e7dea2d6cf0a
DESKTOP-HHJ9SIF\user <shg99sora@yahoo.co.jp>
2019-01-26 13:14:53 +0900

    ログイン　テスト

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
 // 会員登録
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
 
